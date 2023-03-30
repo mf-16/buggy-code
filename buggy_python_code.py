9eea24c321394e978489fb5c17b7a66536229ea9
unknown <xforman3@ad.fi.muni.cz>
2023-03-30 12:57:32 +0200

    more more changes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,3 @@
-import sys
-import os
 import yaml
 import flask
 
@@ -29,12 +27,6 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
-
-    try:
-        http = urllib.PoolManager()
-        r = http.request('GET', url)
-    except:
-        print('Exception')
 
 
 def load_yaml(filename):
